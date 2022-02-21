84a3f482534d9c28939f29f3cb038639692d2f42
Francis Araujo <francis.araujo@gmail.com>
2022-02-21 20:15:40 -0300

    Fixing permissions

--- log-management/install.sh
+++ log-management/install.sh
@@ -6,6 +6,7 @@ kubectl apply -f log-management/elasticsearch.yml \
                 -f log-management/logstash.yml \
                 -f log-management/kibana.yml  \
                 -f log-management/metricbeat.yml  \
+                -f log-management/heartbeat.yml  \
                 -f log-management/filebeat.yml \
                 -f log-management/kube-state-metrics.yml  \
                 -f log-management/curator.yml 
--- log-management/uninstall.sh
+++ log-management/uninstall.sh
@@ -6,8 +6,9 @@ kubectl delete -f log-management/elasticsearch.yml \
                 -f log-management/logstash.yml \
                 -f log-management/kibana.yml  \
                 -f log-management/metricbeat.yml  \
-                -f log-management/filebeat.yml 
-                -f log-management/kube-state-metrics.yml \
+                -f log-management/heartbeat.yml  \
+                -f log-management/filebeat.yml \
+                -f log-management/kube-state-metrics.yml  \
                 -f log-management/curator.yml 
 
 echo "Removed ELK logs management"
