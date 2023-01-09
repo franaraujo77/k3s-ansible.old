41faded128c578b4cc24d794d41f155eb88a97dd
Francis Araujo <francis.araujo@gmail.com>
2023-01-08 21:20:48 -0300

    Updating longhorn installation

--- storage/install.sh
+++ storage/install.sh
@@ -2,6 +2,17 @@
 
 echo "Setting up storage management"
 
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml
+
+echo "Type password"
+read -s PASSWORD
+
+USER=admin; echo "${USER}:$(openssl passwd -stdin -apr1 <<< ${PASSWORD})" >> auth
+
+kubectl -n longhorn-system create secret generic basic-auth --from-file=auth
+
 kubectl apply -f storage/longhorn.yml 
+
+rm auth
 
 echo "Finished storage management setup"
--- storage/uninstall.sh
+++ storage/uninstall.sh
@@ -2,6 +2,8 @@
 
 echo "Removing storage management"
 
+kubectl delete -f https://raw.githubusercontent.com/longhorn/longhorn/master/deploy/longhorn.yaml
+
 kubectl delete -f storage/longhorn.yml 
 
 echo "Removed storage management"
