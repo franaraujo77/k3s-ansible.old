a635858e07baf834d128a58e5f353c48a075fa6e
Francis Araujo <francis.araujo@gmail.com>
2023-01-30 08:40:13 -0300

    Migrating dashboard to Heimdall

--- dashboard/install.sh
+++ dashboard/install.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-echo "Setting up kubernetes dashboard"
+echo "Setting up dashboard"
 
-GITHUB_URL=https://github.com/kubernetes/dashboard/releases
-VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
-kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+kubectl apply -f dashboard/dashboard.yml 
 
-kubectl apply -f dashboard/dashboard.admin-user-role.yml \
-                -f dashboard/dashboard.admin-user.yml \
-                -f dashboard/dashboard.ingress.yml \
-                -f dashboard/dashboard.traefik-insecure.yml
-
-echo "Finished kubernetes dashboard setup"
+echo "Finished dashboard setup"
--- dashboard/uninstall.sh
+++ dashboard/uninstall.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-echo "Removing kubernetes dashboard"
+echo "Removing dashboard"
 
-GITHUB_URL=https://github.com/kubernetes/dashboard/releases
-VERSION_KUBE_DASHBOARD=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o /dev/null | sed -e 's|.*/||')
-kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/${VERSION_KUBE_DASHBOARD}/aio/deploy/recommended.yaml
+kubectl delete -f dashboard/dashboard.yml 
 
-kubectl delete -f dashboard/dashboard.admin-user-role.yml \
-                -f dashboard/dashboard.admin-user.yml \
-                -f dashboard/dashboard.ingress.yml \
-                -f dashboard/dashboard.traefik-insecure.yml
-
-echo "Removed kubernetes dashboard setup"
+echo "Removed dashboard setup"
